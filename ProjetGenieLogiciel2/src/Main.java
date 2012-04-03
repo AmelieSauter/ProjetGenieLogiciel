61a2baa6b232c3564e954a72855b453ff5236f18
Administrateur <Administrateur@nom-9b46a04b692>
2012-04-03 13:11:35 +0200

    elo

--- ProjetGenieLogiciel2/src/Main.java
+++ ProjetGenieLogiciel2/src/Main.java
@@ -4,6 +4,18 @@
  */
 public class Main {
     public static void main(String[] args){
-        System.out.println("Test");
+        Station station1 = new Station("station1",10,20);
+        Station station2 = new Station("station2",10,5);
+        Station station3 = new Station("station3",10,-15);
+        Station station4 = new Station("station4",0,20);
+        Station station5 = new Station("station5",20,20);
+        
+        Ligne ligne1= new Ligne("ligne1");
+        Ligne ligne2= new Ligne("ligne2");
+        Ligne ligne3= new Ligne("ligne3");
+        Ligne ligne4= new Ligne("ligne4");
+        Ligne ligne5= new Ligne("ligne5");
+        
+        System.out.println("Deuxième commit");
     }
 }
